7595ee4171c41a60cee289b3206a5166ecc05149
UP240960 <up240960@alumnos.upa.edu.mx>
2025-06-05 10:49:38 -0600

    ejercicio de comandos git

---
+++ Hello.py
@@ -0,0 +1,47 @@
+print("Hello World!");
+print('''
+>>> 3+4
+7
+>>> 4-3
+1
+>>> 3*4
+12
+>>> 3%4
+3
+>>> 4%3
+1
+>>> 4/3
+1.3333333333333333
+>>> 3**4
+81
+>>> 3//4
+0
+>>> 4//3
+1
+>>> 'Your name'
+'Your name'
+>>> "Your name"
+'Your name'
+>>> 'Your Family name'
+'Your Family name'
+>>> 'Your country'
+'Your country'
+>>> 'I am enjoying 30 days of python'
+'I am enjoying 30 days of python'
+>>> type(10)
+<class 'int'>
+>>> type(9.8)
+<class 'float'>
+>>> type(3.14)
+<class 'float'>
+>>> type(4-4j)
+<class 'complex'>
+>>> type(['Asabeneh','Python','Finland'])
+<class 'list'>
+>>> type('Your name')
+<class 'str'>
+>>> type('Your family name')
+<class 'str'>
+>>> type('Your country')
+<class 'str'>
+''')
