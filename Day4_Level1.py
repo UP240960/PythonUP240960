dc36f3e39a4f64f7bb112ee98bbd0810853889dc
UP240960 <up240960@alumnos.upa.edu.mx>
2025-06-19 10:21:05 -0600

    day 4

--- Day4_Level1.py
+++ Day4_Level1.py
@@ -55,7 +55,53 @@ print("El ultimo caracter es:", company[-1])
 #Ejercicio 17
 print("El caracter 10 es:", company[10])
 #Ejercicio 18
-print("El acronimo para (coding for all) es ","".join(palabra[0] for palabra in company.split()))
+print('El acronimo para "Coding For All" es ',"".join(palabra[0] for palabra in company.split()))
 #Ejercicio 19
-print("El acronimo de (Python for all)", "".join(palabra[0] for palabra in company2.split()))
+print('El acronimo de "Python for all"', "".join(palabra[0] for palabra in company2.split()))
 #Ejercicio 20
+print('La posición de "C" es: ', company.index("C"))
+#Ejercicio 21
+print('La posicionde "F" es: ', company.index("F"))
+#Ejercicio 22
+cfap= "Coding For All People"
+print("La ultima vez que l  aparece es en la posición: ", cfap.rfind("l"))
+#Ejercicio 23
+sentence= 'You cannot end a sentence with "because", because "because" is a conjunction'
+print('La primera vez que aparece la palabra "because" es en: ', sentence.index("because"))
+#Ejercicio 24
+print('La ultima vez que la palabra "because" es en:', sentence.rindex("because"))
+#Ejercicio 25
+becuase= '"because", because "because"'
+print(sentence.replace(becuase,"").strip())
+#Ejercicio 26
+print('La primera vez que "because" aparece es en:', sentence.find("because"))
+#Ejercicio 27
+print(sentence.replace(becuase,"").strip())
+#Ejercicio 28
+print(company.startswith("Coding"))
+#Ejercicio 29
+print(company.endswith("coding"))
+#Ejercicio 30
+espacios="  Coding For All   "
+print(espacios.strip())
+#Ejercicio 31
+DP= "30DaysOfPython"
+TDP= "thirty_days_of_python"
+print(DP.isidentifier())
+print(TDP.isidentifier())
+#Ejercicio 32
+names=["Django", "Flask", "Bottle", "Pyramid", "Falcon"]
+print("#".join(names))
+print(" ".join(names))
+#Ejercicio 33
+print("I am enjoying this challege.\nI jsut wonder what is next")
+#Ejercicio 34
+print("Name\tAge\tCountry\tCity")
+print("Asbeneh\t250\tFinland\tHelsinki")
+#Ejercicio 35
+radius = 10
+area = 3.14 * radius ** 2
+print("radio = ", radius)
+print("area = ", area)
+print(f"El area de un circulo con un radio de {radius}, es {int(area)} metros cuadrados")
+#Ejercicio 36
