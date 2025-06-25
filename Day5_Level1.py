32213296bcd79c1981177d5aac0c65649b40cbb1
UP240960 <up240960@alumnos.upa.edu.mx>
2025-06-25 10:54:20 -0600

    :p

---
+++ Day5_Level1.py
@@ -0,0 +1,29 @@
+#Ejercicio 1
+lista = []
+#Ejercicio 2
+abc = ["a" , "b" , "c" , "d" , "e"]
+#Ejercicio 3
+print("Lista #1: " , len(lista))
+print("Lista #2 :" , len(abc))
+#Ejercicio 4
+print("Primer item:", abc[0], "\nItem intermedio:", abc[len(abc) // 2],"\nUltimo item", abc[-1])
+#Ejercicio 5
+mixed_data_types = ["Alan", 23 , 1.87 , "Single" , "Buenaventura #520, col. Puentes"]
+#Ejercicio 6
+it_companies = ["Facebook" , "Google" , "Microsoft" , "Apple" , "IBM" , "Oracle" , "Amazon"]
+#Ejercicio 7
+print(it_companies)
+#Ejercicio 8
+print(len(it_companies))
+#Ejercicio 9
+print("Primer compañia: " , it_companies[0] , "\nCompañia de en medio: " , it_companies[len(it_companies) // 2] , "\nUltima compañia: " , it_companies[-1])
+#Ejercicio 10
+it_companies[0] = "Nintendo"
+print(it_companies)
+#Ejercicio 11
+it_companies.append("Netflix")
+print(it_companies)
+#Ejercicio 12
+it_companies.insert(len(it_companies)//2, "Samsung")
+print(it_companies)
+#Ejercicio 13
