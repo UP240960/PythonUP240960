f0f20366fed8f30782ed4e40fb77cc43f7679964
UP240960 <up240960@alumnos.upa.edu.mx>
2025-07-15 13:48:37 -0600

    avance

---
+++ Day6_Level1.py
@@ -0,0 +1,22 @@
+#Ejercicio 1
+vacio= ()
+print(vacio)
+
+#Ejercicio 2
+Hermanas= ("Karla" , "Vanessa" , "Magdalena")
+Hermano= ("Daniel" , )
+print(Hermanas)
+print(Hermano)
+
+#Ejercicio 3
+siblings = Hermanas + Hermano
+print(siblings)
+
+#Ejercicio 4
+total= len(siblings)
+print(f'Tengo un total de {total} herman@s')
+
+#Ejercicio 5
+padres= ("Gloria" , "Juan")
+family_members= siblings + padres
+print(family_members)
---
+++ Day6_Level2.py
@@ -0,0 +1,10 @@
+#Ejercicio 1
+Hermanas= ("Karla" , "Vanessa" , "Magdalena")
+Hermano= ("Daniel" , )
+siblings = Hermanas + Hermano
+padres= ("Gloria" , "Juan")
+family_members= siblings + padres
+*siblings , madre, padre = family_members
+print(f'Mis hermanos son: {siblings}')
+print(f'mis padres son: {padre} y {madre}')
+#Ejerciio 2
